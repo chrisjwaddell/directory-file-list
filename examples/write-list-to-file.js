b69739b22529c26432fbd5777c97ad3e41fc3850
Chris Waddell <yesitschris@hotmail.com>
2023-08-17 23:43:21 +1000

    [patch] Relative root path was giving relative path as output, now it gives absolute path

--- examples/write-list-to-file.js
+++ examples/write-list-to-file.js
@@ -1,34 +1,39 @@
 // Usage: node ./examples/write-list-to-file.js "/home" ".txt"
-const { dirFileList } = require("../src/dirFileList.js");
-const fs = require("fs");
-const process = require("process");
+const {dirFileList} = require("../src/dirFileList.js")
+const fs = require("fs")
+const process = require("process")
 const path = require("path")
 
 ;(async function () {
-    let rootDir = process.argv[2]
+	let rootDir = process.argv[2]
 
-    let toString = Object.prototype.toString;
+	let toString = Object.prototype.toString
 
-    if (toString.call(process.argv[3]) !== "[object String]") {
-        console.log("extensions is wrong type. It should be String")
-        return 0
-    }
+	if (toString.call(process.argv[3]) !== "[object String]") {
+		console.log("extensions is wrong type. It should be String")
+		return 0
+	}
 
-    let ext = [process.argv[3]]
+	let ext = [process.argv[3]]
 
-    let result = await dirFileList(rootDir, ext, {recursive: true, exclude_directories: [ "node_modules", ".git", "archive" ] })
-    .catch(err => {
-        console.log(err)
-    })
+	let result = await dirFileList(rootDir, ext, {
+		recursive: true,
+		exclude_directories: ["node_modules", ".git", "archive"],
+	}).catch((err) => {
+		console.log(err)
+	})
 
-    // Write to a file
-    fs.writeFileSync(path.join("dist", "output.json"), JSON.stringify(result, null, 2), err => {
-        if (err) {
-            console.log("Error")
-            console.error(err)
-        } else {
-            console.log("File successfully written!")
-        }
-    })
-
+	// Write to a file
+	fs.writeFileSync(
+		path.join("dist", "output.json"),
+		JSON.stringify(result, null, 2),
+		(err) => {
+			if (err) {
+				console.log("Error")
+				console.error(err)
+			} else {
+				console.log("File successfully written!")
+			}
+		}
+	)
 })()
--- src/dirFileList.js
+++ src/dirFileList.js
@@ -1,158 +1,176 @@
-const fs = require('node:fs/promises');
-const st = require('node:fs/promises');
+const fs = require("node:fs/promises")
+const st = require("node:fs/promises")
 const path = require("path")
 
-
 async function dirFileList(rootPath, extensions, options) {
-    // ^INITIALIZE
+	// ^INITIALIZE
 	let settings = options || {}
-    settings.extensions = extensions ?? []
+	settings.extensions = extensions ?? []
 	settings.recursive = settings.recursive ?? true
-    settings.exclude_filenames = options.exclude_filenames ?? []
-    settings.exclude_directories = options.exclude_directories ?? []
-    settings.max_depth = settings.max_depth ?? 15
+	settings.exclude_filenames = options.exclude_filenames ?? []
+	settings.exclude_directories = options.exclude_directories ?? []
+	settings.max_depth = settings.max_depth ?? 15
 
-    let toString = Object.prototype.toString
-    if (toString.call(settings.extensions) !== "[object Array]") {
-        return Promise.reject("extensions is wrong type. It should be Array")
-    }
-    if (toString.call(settings.recursive) !== "[object Boolean]") {
-        return Promise.reject("recursive is wrong type. It should be Boolean")
-    }
-    if (toString.call(settings.exclude_filenames) !== "[object Array]") {
-        return Promise.reject("exclude_filenames is wrong type. It should be Array")
-    }
-    if (toString.call(settings.exclude_directories) !== "[object Array]") {
-        return Promise.reject("exclude_directories is wrong type. It should be Array")
-    }
-    if (toString.call(settings.max_depth) !== "[object Number]") {
-        return Promise.reject("max_depth is wrong type. It should be Number")
-    }
+	let toString = Object.prototype.toString
+	if (toString.call(settings.extensions) !== "[object Array]") {
+		return Promise.reject("extensions is wrong type. It should be Array")
+	}
+	if (toString.call(settings.recursive) !== "[object Boolean]") {
+		return Promise.reject("recursive is wrong type. It should be Boolean")
+	}
+	if (toString.call(settings.exclude_filenames) !== "[object Array]") {
+		return Promise.reject(
+			"exclude_filenames is wrong type. It should be Array"
+		)
+	}
+	if (toString.call(settings.exclude_directories) !== "[object Array]") {
+		return Promise.reject(
+			"exclude_directories is wrong type. It should be Array"
+		)
+	}
+	if (toString.call(settings.max_depth) !== "[object Number]") {
+		return Promise.reject("max_depth is wrong type. It should be Number")
+	}
 
-    let fileList = await filesDirs(rootPath, rootPath, 1, settings)
+	let fileList = await filesDirs(rootPath, rootPath, 1, settings)
 
-    // filter out extensions
-    let extList = []
-    if (settings.extensions.length) {
-        extList = fileList.filter(file => {
-            return settings.extensions.includes(file.extension)
-        })
-    } else {
-        extList = fileList
-    }
+	// filter out extensions
+	let extList = []
+	if (settings.extensions.length) {
+		extList = fileList.filter((file) => {
+			return settings.extensions.includes(file.extension)
+		})
+	} else {
+		extList = fileList
+	}
 
-    return extList
+	return extList
 }
-
 
 async function filesDirs(rootPath, currentPath, depth, settings) {
-    let arrResult = []
+	let arrResult = []
 
-    // if this current directory is in the exclude_directories list, return
-    if (settings.exclude_directories.includes(path.basename(currentPath))) return arrResult
+	// if this current directory is in the exclude_directories list, return
+	if (settings.exclude_directories.includes(path.basename(currentPath)))
+		return arrResult
 
-    let dirList = await getDirectoryList(currentPath)
+	let dirList = await getDirectoryList(currentPath)
 
-    // filter out exclude_filenames
-    let afterExclFiles = []
+	// filter out exclude_filenames
+	let afterExclFiles = []
 
-    if (settings.exclude_filenames.length) {
-        afterExclFiles = dirList.filter(file => settings.exclude_filenames.includes(file) === false)
-    } else {
-        afterExclFiles = dirList
-    }
+	if (settings.exclude_filenames.length) {
+		afterExclFiles = dirList.filter(
+			(file) => settings.exclude_filenames.includes(file) === false
+		)
+	} else {
+		afterExclFiles = dirList
+	}
 
+	let arrFileStatsPromises = afterExclFiles.map((cv) =>
+		getFileStatsWrapper(currentPath, cv, rootPath, depth)
+	)
 
-    let arrFileStatsPromises = afterExclFiles.map(cv => getFileStatsWrapper(currentPath, cv, rootPath, depth))
+	let arrFileStats = await Promise.all(arrFileStatsPromises).catch((err) => {
+		console.log(err)
+	})
 
-    let arrFileStats = await Promise.all(arrFileStatsPromises).catch(err => {
-        console.log(err)
-    })
+	let arrFileStatsNeeded = arrFileStats.map((cv, i) => {
+		let f
+		try {
+			f = cv.isFile()
+		} catch {
+			f = {}
+		}
 
+		return {
+			...getFileStatsNeeded(
+				cv,
+				depth,
+				path.resolve(path.join(currentPath, afterExclFiles[i])),
+				rootPath
+			),
+			filepath: path.resolve(path.join(currentPath, afterExclFiles[i])),
+			file: f,
+		}
+	})
 
-    let arrFileStatsNeeded = arrFileStats.map((cv, i) => {
-        let f
-        try {
-            f = cv.isFile()
-        }
-        catch {
-            f = {}
-        }
+	const files = arrFileStatsNeeded.filter((item) => item.file)
+	const dirs = arrFileStatsNeeded.filter((item) => !item.file)
 
-        return { ...getFileStatsNeeded(cv, depth, path.join(currentPath, afterExclFiles[i]), rootPath), filepath: path.join(currentPath, afterExclFiles[i]), file: f }
-    })
+	files.forEach((cv) => arrResult.push(cv))
 
-    const files = arrFileStatsNeeded.filter(item => item.file)
-    const dirs = arrFileStatsNeeded.filter(item => !item.file)
+	let filesListDirs = []
+	if (dirs.length) {
+		if (settings.recursive) {
+			if (settings.max_depth > depth) {
+				filesListDirs = dirs.map((dir) =>
+					filesDirs(rootPath, dir.filepath, depth + 1, settings)
+				)
+			}
+		}
+	} else {
+		return arrResult
+	}
 
-    files.forEach(cv => arrResult.push(cv))
+	let subdirs = await Promise.all(filesListDirs)
+	subdirs.flat().forEach((cv) => arrResult.push(cv))
 
-    let filesListDirs = []
-    if (dirs.length) {
-
-        if (settings.recursive) {
-            if (settings.max_depth > depth) {
-                filesListDirs = dirs.map(dir => filesDirs(rootPath, dir.filepath, depth + 1, settings))
-            }
-        }
-    } else {
-        return arrResult
-    }
-
-
-    let subdirs = await Promise.all(filesListDirs)
-    subdirs.flat().forEach(cv => arrResult.push(cv))
-
-    return arrResult
+	return arrResult
 }
 
-
-
-function getDirectoryList( directory ) {
-    return new Promise((resolve, reject) => {
-        let f = fs.readdir(
-            directory,
-            function ( err, filenames ) {
-                if (err) {
-                    reject( err )
-                }
-            }
-        )
-        resolve( f )
-    })
+function getDirectoryList(directory) {
+	return new Promise((resolve, reject) => {
+		let f = fs.readdir(directory, function (err, filenames) {
+			if (err) {
+				reject(err)
+			}
+		})
+		resolve(f)
+	})
 }
-
 
 function getFileStatsNeeded(filestats, depth, filepath, rootPath) {
-    return { size: filestats.size, mode: filestats.mode, access_time: filestats.atime, modified_time: filestats.mtime, status_change_time: filestats.ctime, birthtime: filestats.birthtime, depth, filename: path.basename(filepath), relative: filepath.replace(rootPath + "\\", ""), dir: path.dirname(filepath), extension: path.extname(filepath) }
+	return {
+		size: filestats.size,
+		mode: filestats.mode,
+		access_time: filestats.atime,
+		modified_time: filestats.mtime,
+		status_change_time: filestats.ctime,
+		birthtime: filestats.birthtime,
+		depth,
+		filename: path.basename(filepath),
+		relative: filepath.replace(path.resolve(rootPath) + "\\", ""),
+		dir: path.dirname(filepath),
+		extension: path.extname(filepath),
+	}
 }
 
-
 function getFileStats(file) {
-    return new Promise(async (resolve, reject) => {
-        let f = st.stat(
-            file,
-            ( err ) => {
-                reject( err )
-            }
-        )
-        resolve( f )
-    })
+	return new Promise(async (resolve, reject) => {
+		let f = st.stat(file, (err) => {
+			reject(err)
+		})
+		resolve(f)
+	})
 }
 
 // If the filesystem can't return stat,
 // return an simple object
 // To avoid Promise.all failing
 async function getFileStatsWrapper(currentPath, filename, rootPath, depth) {
-    let r = await getFileStats(path.join(currentPath,filename))
-        .then(result => result)
-        .catch(err => {
-            return { filename, depth, filepath: path.join(currentPath, filename), dir: currentPath }
-        })
+	let r = await getFileStats(path.join(currentPath, filename))
+		.then((result) => result)
+		.catch((err) => {
+			return {
+				filename,
+				depth,
+				filepath: path.join(currentPath, filename),
+				dir: currentPath,
+			}
+		})
 
-    return r
+	return r
 }
 
-
-
-module.exports = { dirFileList, getDirectoryList }
+module.exports = {dirFileList, getDirectoryList}
